cf9ea89fd15bf974fd2e7e5931bebfbf167842f3
Pradeep <pradeep.ktg@gmail.com>
2015-04-27 14:41:08 +0530

    Initial Commit

---
+++ gitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitTest
+//
+//  Created by Pradeep Chandrasekaran on 27/04/15.
+//  Copyright (c) 2015 Pradeep Chandrasekaran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitTest
+//
+//  Created by Pradeep Chandrasekaran on 27/04/15.
+//  Copyright (c) 2015 Pradeep Chandrasekaran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
