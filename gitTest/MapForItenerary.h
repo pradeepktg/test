c7115a82ff69d83cfeb1be32d3d12ad264b4afe4
Pradeep <pradeep.ktg@gmail.com>
2015-04-27 15:22:11 +0530

    For the second version

---
+++ gitTest/MapForItenerary.h
@@ -0,0 +1,13 @@
+//
+//  MapForItenerary.h
+//  gitTest
+//
+//  Created by Pradeep Chandrasekaran on 27/04/15.
+//  Copyright (c) 2015 Pradeep Chandrasekaran. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MapForItenerary : NSObject
+
+@end
---
+++ gitTest/MapForItenerary.m
@@ -0,0 +1,13 @@
+//
+//  MapForItenerary.m
+//  gitTest
+//
+//  Created by Pradeep Chandrasekaran on 27/04/15.
+//  Copyright (c) 2015 Pradeep Chandrasekaran. All rights reserved.
+//
+
+#import "MapForItenerary.h"
+
+@implementation MapForItenerary
+
+@end
